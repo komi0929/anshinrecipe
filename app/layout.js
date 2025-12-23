8937e89fc4d077680da32ccb4b219a67a32ab4bf
komi0929 <y.kominami@hitokoto1.co.jp>
2025-12-24 06:04:58 +0900

    Add OGP image for social media sharing

--- app/layout.js
+++ app/layout.js
@@ -21,11 +21,20 @@ export const metadata = {
         siteName: 'あんしんレシピ',
         locale: 'ja_JP',
         type: 'website',
+        images: [
+            {
+                url: 'https://anshinrecipe.com/ogp.jpg',
+                width: 1200,
+                height: 630,
+                alt: 'あんしんレシピ - アレルギーっ子のためのレシピ共有アプリ',
+            },
+        ],
     },
     twitter: {
         card: 'summary_large_image',
         title: 'あんしんレシピ',
         description: '食物アレルギーを持つお子様のための、安心レシピ共有アプリ',
+        images: ['https://anshinrecipe.com/ogp.jpg'],
     },
     manifest: '/manifest.json',
     appleWebApp: {
